cd0e6e6867c008b1e9d432c1ed00454be1497755
陈斌 <wzdxc123@163.com>
2015-12-30 17:19:23 +0800

    集成GetuiSDK，新增解析透传消息的工具类

--- Example/LogExample/AppDelegate.h
+++ Example/LogExample/AppDelegate.h
@@ -7,11 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GeTuiSdk.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+/// 个推开发者网站中申请App时注册的AppId、AppKey、AppSecret
+#define kGtAppId           @"RDQVXgdN2YAFtMFw3a9v4"
+#define kGtAppKey          @"Cb6XvcFiuu5cvo6e90pr88"
+#define kGtAppSecret       @"kxtY9iJwLz9NwJof1R7ya"
+
+/// 需要使用个推回调时，需要添加"GeTuiSdkDelegate"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, GeTuiSdkDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
